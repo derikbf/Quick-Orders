7785a5077e8288ca74b2eba4e9059b98c47cb7d1
Rafael Shintani <rhiroshi14@hotmail.com>
2022-02-16 19:44:12 -0300

    Arrumado paths do devise e removido sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :role, presence: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
