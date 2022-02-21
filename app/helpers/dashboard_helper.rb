ab00459fd79cb5c228e091c62921b314b6539d15
Rafael Shintani <rhiroshi14@hotmail.com>
2022-02-20 21:55:51 -0300

    Added comments

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,4 +1,22 @@
 module DashboardHelper
+
+  # Created HTML for dashboard cards as follows:
+  # <div class="col-xl-3 col-md-6 mb-4">
+  #   <div class="card border-left-primary shadow h-100 py-2 border border-white bg-dark">
+  #     <div class="card-body">
+  #       <div class="row no-gutters align-items-center">
+  #         <div class="col mr-2">
+  #           <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+  #             <<TITULO>>
+  #           </div>
+  #           <div class="h5 mb-0 font-weight-bold text-gray-800">
+  #             <<CONTEUDO>>
+  #           </div>
+  #         </div>
+  #       </div>
+  #     </div>
+  #   </div>
+  # </div>
   def dashboard_card(title, &block)
     content_tag(:div, class: "col-xl-3 col-md-6 mb-4") do
       content_tag(:div, class: 'card border-left-primary shadow h-100 py-2 border border-white bg-dark') do
